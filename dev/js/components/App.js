7df261d0a5eed9242839ca190fceaff3b36f4473
majkamajka <maja.miri@gmail.com>
2017-12-25 19:39:03 +0100

    first reducer, reducer_users

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import UserList from '../containers/user-list';
-import UserDetails from '../containers/user-detail';
+import UserList from '../containers/user-list.js';
 require('../../scss/style.scss');
 
 const App = () => (
-    <div>
-        <h2>User List</h2>
-        <UserList />
-        <hr />
-        <h2>User Details</h2>
-        <UserDetails />
-    </div>
+  
+  <div>
+    <h2>username list:</h2>
+    <UserList />
+    <h3>user details:</h3>
+  </div>
 );
 
 export default App;
